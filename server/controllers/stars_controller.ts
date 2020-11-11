7144781314a577737406ec70d7b1c2e673137ea5
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2020-11-11 16:58:44 +0900

    type fix

--- server/controllers/stars_controller.ts
+++ server/controllers/stars_controller.ts
@@ -2,7 +2,7 @@ import express from "express"
 const Stars = require("../models/stars_model");
 
 module.exports = {
-  GetStars: function(req, res) {
+  GetStars: function(req: any, res: any) {
     const array = Stars.getIndex()
     res.send(JSON.stringify({
       Result: array
