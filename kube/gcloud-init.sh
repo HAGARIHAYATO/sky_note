af1aa30132fdd18473304343fc870fac849df5dc
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2020-11-13 23:04:21 +0900

    add: shell

--- kube/gcloud-init.sh
+++ kube/gcloud-init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-export PROJECT_ID=${gcloud config get-value project}
+export PROJECT_ID=kubernetes-practice-293800
+export MY_ZONE=us-central1-c
 gcloud builds submit --tag gcr.io/${PROJECT_ID}/sky_note .
 gcloud container clusters create sky_note \
   --num-nodes 1 \
   --enable-basic-auth \
   --issue-client-certificate \
-  --zone us-central1-c
+  --zone ${MY_ZONE}
