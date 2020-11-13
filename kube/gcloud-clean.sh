5a01d115576bc944477546d288a6dc1acfcfa751
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2020-11-13 23:34:06 +0900

    add: shell

--- kube/gcloud-clean.sh
+++ kube/gcloud-clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcloud container clusters delete sky-note --zone us-central1-c
-gcloud container images delete gcr.io/kubernetes-practice-293800/sky-note
+gcloud container images delete gcr.io/kubernetes-practice-293800/sky-note:v1
